fe9890c2e97bf87a8a19341225657d4b425bf6cc
Minhdz2004 <nguyenvanminh2004.bg@gmail.com>
2024-10-26 14:55:41 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace form_tra_cuusv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtmasv = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // txtmasv
+            // 
+            txtmasv.Location = new Point(234, 124);
+            txtmasv.Name = "txtmasv";
+            txtmasv.Size = new Size(261, 27);
+            txtmasv.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(293, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Tra cứu sinh viên";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(516, 122);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Tra cứu";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(234, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(350, 85);
+            label2.TabIndex = 0;
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(txtmasv);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtmasv;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+    }
+}
